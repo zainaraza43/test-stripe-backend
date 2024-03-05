9452627c458af69b3c267a67bed1b207e9aa2fba
zainaraza43 <urboyzain@gmail.com>
2024-03-04 21:03:53 -0500

    backend makes a PaymentIntent

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
-from flask import Flask
+import stripe
+import os
+
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
 app = Flask(__name__)
 
@@ -6,6 +14,19 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment_intent():
+    try:
+        payment_intent = stripe.PaymentIntent.create(
+            amount=1099,
+            currency='eur',
+            automatic_payment_methods={'enabled': True},
+        )
+
+        return jsonify(payment_intent), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 400
+
 if __name__ == '__main__':
     app.run(debug=True)
-
