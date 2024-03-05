0585bdac0a64cefaf08e61bf6509ad038a13ce43
zainaraza43 <urboyzain@gmail.com>
2024-03-04 21:15:08 -0500

    added payment-sheet from stripe example

--- server.py
+++ server.py
@@ -28,5 +28,30 @@ def create_payment_intent():
     except Exception as e:
         return jsonify(error=str(e)), 400
 
+@app.route('/payment-sheet', methods=['POST'])
+def payment_sheet():
+  # Use an existing Customer ID if this is a returning customer
+  customer = stripe.Customer.create()
+  ephemeralKey = stripe.EphemeralKey.create(
+    customer=customer['id'],
+    stripe_version='2023-10-16',
+  )
+  paymentIntent = stripe.PaymentIntent.create(
+    amount=1099,
+    currency='eur',
+    customer=customer['id'],
+    # In the latest version of the API, specifying the `automatic_payment_methods` parameter
+    # is optional because Stripe enables its functionality by default.
+    automatic_payment_methods={
+      'enabled': True,
+    },
+  )
+  return jsonify(paymentIntent=paymentIntent.client_secret,
+                 ephemeralKey=ephemeralKey.secret,
+                 customer=customer.id,
+                 publishableKey='pk_test_51Oq22ZP44uLDl7eGtNaHbbthCuhiZtnG3Y7F13wHrKYclXYVEmnfpZIWGMVeXwUqR2DEoDec6xpQcKQYvzeOHUgu00o91Y8ZVK'
+
+)
+
 if __name__ == '__main__':
     app.run(debug=True)
